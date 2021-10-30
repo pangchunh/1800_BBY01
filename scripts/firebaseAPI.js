02955f4584e1072d0fd34fc7039480de62877e1a
deadeye167 <knguyen101@my.bcit.ca>
2021-10-30 14:12:30 -0700

    Adding login page with firebase

---
+++ scripts/firebaseAPI.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCUCQIGseWvT3gfpH2OsgTOfArKOhqg-IY",
+    authDomain: "project-1800-10643.firebaseapp.com",
+    projectId: "project-1800-10643",
+    storageBucket: "project-1800-10643.appspot.com",
+    messagingSenderId: "649427147665",
+    appId: "1:649427147665:web:2c6dbbad2f35e1fc171830"
+  };
+  
+  // Initialize Firebase
+  const app = firebase.initializeApp(firebaseConfig);
